b415d89b1d2a76016496c59bd365b45c248df52f
Shailendra Maheshwari <8maheshw@gmail.com>
2025-08-25 16:31:54 +0530

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,9 @@ $password = "";
 $db = "onlineshop";
 
 // Create connection
-$con = mysqli_connect($servername, $username, $password,$db);
+//$con = mysqli_connect($servername, $username, $password,$db);
+$password = getenv('MYSQL_SECURE_PASSWORD');
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if (!$con) {
@@ -15,3 +17,4 @@ if (!$con) {
 
 
 ?>
+
